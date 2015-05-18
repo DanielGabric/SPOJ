4e404d60077e72c77c752bf5f9860210efa11884
Dan <danielgabric@live.com>
2015-05-18 19:39:35 -0400

    .

--- Feynman.c
+++ Feynman.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 int main(){
-	int i;
-	while(1){
-		scanf("%d",&i);
-		if(i==0)break;
-		printf("%d\n",(i*(i+1)*(2*i+1))/6);
-	}
-	return 0;
+    int i;
+    while(1){
+        scanf("%d",&i);
+        if(i==0)break;
+        printf("%d\n",(i*(i+1)*(2*i+1))/6);
+    }
+    return 0;
 }
